54ab15edcf8156cd0ec2894ed3b3b1218b33c0c0
hyeonproject <axzswq@gmail.com>
2023-03-26 23:52:37 +0900

    fix: 라이브러리 및 환경 개선

--- app/chat_domain/chat.py
+++ app/chat_domain/chat.py
@@ -5,8 +5,17 @@ class ChatDalCog(commands.Cog, name='Chat GPT Cog'):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
+        self._channel_id = None
+
+    @property
+    def last_member(self):
+        return self._last_member
+
+    @property
+    def channel_id(self):
+        return self._channel_id
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author != self.bot.user:
+    async def answer_on_message(self, message):
+        if (message.author != self.bot.user) and (message.channel == self._channel_id):
             pass
--- app/service/chatgpt.py
+++ app/service/chatgpt.py
@@ -1,14 +1,22 @@
 import openai
 
+from app.service.secret import get_secret
+
+openai.api_key = get_secret("openai_api_key")
+
 
 class ChatGptApi:
     def __init__(self):
-        self.chat_api = openai.ChatCompletion.create(
-            model='gpt-4',
-            message=[
+        self._model = 'gpt-3.5-turbo'
+    @property
+    def model(self):
+        return self.model
 
-            ]
+    async def answer_message(self, message: str) -> str:
+        response = openai.Completion.create(
+            model=self._model,
+            message=message,
         )
 
-    async def answer_chat(self, message: str) -> str:
+    async def num_tokens_from_messages(self, messages, model='gpt-3.5'):
         pass
